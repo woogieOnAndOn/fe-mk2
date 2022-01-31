73b41e0e793bc60665a6add6b93cb870288f32ea
woogieOnAndOn <123wodnr@naver.com>
2022-01-31 16:30:52 +0900

    [트리] 자동개행 라이브러리 remarkBreaks추가

--- src/util/Parser.util.ts
+++ src/util/Parser.util.ts
@@ -3,12 +3,18 @@ import remarkParse from 'remark-parse'
 import remarkRehype from 'remark-rehype'
 import rehypeSanitize from 'rehype-sanitize'
 import rehypeStringify from 'rehype-stringify'
+import remarkBreaks from 'remark-breaks'
 import remarkGfm from 'remark-gfm'
 
 const parseMd = async (contentMd: string) => {
   const parsedText = await unified()
     .use(remarkParse)
-    .use(remarkGfm, {singleTilde: false})
+    .use(remarkBreaks) // 개행 자동
+    .use(remarkGfm, {
+      singleTilde: false, // default true(~취소선~), false(~~취소선~~)
+      tableCellPadding: false, // default true(Serialize tables with a space between delimiters (|) and cell content)
+      tablePipeAlign: true, // default true(Serialize by aligning the delimiters (|) between table cells so that they all align nicely and form a grid)
+    }) // ~~취소선~~
     .use(remarkRehype)
     .use(rehypeSanitize)
     .use(rehypeStringify)
