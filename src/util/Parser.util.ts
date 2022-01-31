2d09d7a9321139a0cb506726075126dc25805510
woogieOnAndOn <123wodnr@naver.com>
2022-01-31 16:59:49 +0900

    [트리] html 태그 사용가능토록하는 rehype-raw추가

--- src/util/Parser.util.ts
+++ src/util/Parser.util.ts
@@ -4,6 +4,7 @@ import remarkRehype from 'remark-rehype'
 import rehypeSanitize from 'rehype-sanitize'
 import rehypeStringify from 'rehype-stringify'
 import remarkBreaks from 'remark-breaks'
+import rehypeRaw from 'rehype-raw'
 import remarkGfm from 'remark-gfm'
 
 const parseMd = async (contentMd: string) => {
@@ -15,11 +16,13 @@ const parseMd = async (contentMd: string) => {
       tableCellPadding: false, // default true(Serialize tables with a space between delimiters (|) and cell content)
       tablePipeAlign: true, // default true(Serialize by aligning the delimiters (|) between table cells so that they all align nicely and form a grid)
     }) // ~~취소선~~
-    .use(remarkRehype)
+    .use(remarkRehype, {allowDangerousHtml: true})
+    .use(rehypeRaw)
     .use(rehypeSanitize)
     .use(rehypeStringify)
     .process(contentMd);
 
+  console.log(String(parsedText));
   return String(parsedText);
 }
 
